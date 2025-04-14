f37b1d1b76d5bf583cccc6c4b9d605fdc76830d1
Nicolas <nicolasandrade.de.freitas@gmail.com>
2025-04-14 11:19:26 -0300

    feat: restaurant use cases

--- src/domain/smart-menu/application/use-cases/user/create-user.ts
+++ src/domain/smart-menu/application/use-cases/user/create-user.ts
@@ -1,20 +1,20 @@
-import { Either, left, right } from '@/core/either';
-import { Injectable } from '@nestjs/common';
-import { EmailAlreadyInUseError } from '../__errors/email-already-in-use-error';
-import { User } from '@/domain/smart-menu/enterprise/entities/user';
-import { UsersRepository } from '../../repositories/users-repository';
-import { HashGenerator } from '../../cryptography/hash-generator';
-import { RestaurantsRepository } from '../../repositories/restaurants-repository';
-import { ResourceNotFoundError } from '@/core/errors/errors/resource-not-found-error';
+import { Either, left, right } from '@/core/either'
+import { Injectable } from '@nestjs/common'
+import { EmailAlreadyInUseError } from '../__errors/email-already-in-use-error'
+import { User } from '@/domain/smart-menu/enterprise/entities/user'
+import { UsersRepository } from '../../repositories/users-repository'
+import { HashGenerator } from '../../cryptography/hash-generator'
+import { RestaurantsRepository } from '../../repositories/restaurants-repository'
+import { ResourceNotFoundError } from '@/core/errors/errors/resource-not-found-error'
 
 interface CreateUserUseCaseRequest {
-  email: string;
-  name: string;
-  password: string;
-  restaurantId: string;
+  email: string
+  name: string
+  password: string
+  restaurantId: string
 }
 
-type CreateUserUseCaseResponse = Either<EmailAlreadyInUseError, { user: User }>;
+type CreateUserUseCaseResponse = Either<EmailAlreadyInUseError, { user: User }>
 
 @Injectable()
 export class CreateUserUseCase {
@@ -30,29 +30,29 @@ export class CreateUserUseCase {
     password,
     restaurantId,
   }: CreateUserUseCaseRequest): Promise<CreateUserUseCaseResponse> {
-    const userAlreadyExists = await this.usersRepository.findByEmail(email);
+    const userAlreadyExists = await this.usersRepository.findByEmail(email)
 
     if (userAlreadyExists) {
-      return left(new EmailAlreadyInUseError(email));
+      return left(new EmailAlreadyInUseError(email))
     }
 
-    const restaurant = await this.restaurantsRepository.findById(restaurantId);
+    const restaurant = await this.restaurantsRepository.findById(restaurantId)
 
     if (!restaurant) {
-      return left(new ResourceNotFoundError());
+      return left(new ResourceNotFoundError())
     }
 
-    const hashedPassword = await this.hashGenerator.hash(password);
+    const hashedPassword = await this.hashGenerator.hash(password)
 
     const user = User.create({
       email,
       name,
       password: hashedPassword,
       restaurantId: restaurant.id,
-    });
+    })
 
-    await this.usersRepository.create(user);
+    await this.usersRepository.create(user)
 
-    return right({ user });
+    return right({ user })
   }
 }
